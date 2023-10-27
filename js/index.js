ffe13d979bb84d3f699581369f47b4432f81593b
nicolas <nicolsgonza765@gmail.com>
2023-10-27 12:11:15 -0300

    version 4.2

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const {createApp} = Vue
+const app = createApp({
+    data(){
+        return{
+
+        }
+    },
+    created(){
+
+    },
+    methods:{
+        redirection(){
+            return window.location.href = "https://instagram.com/sofii_gxn?igshid=YTQwZjQ0NmI0OA=="
+        }
+    },
+    computed:{
+
+    }
+})
+app.mount('#app')
