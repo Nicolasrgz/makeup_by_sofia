025245891980bdc48b1e8f31d50fe8a080231c6e
nicolas <nicolsgonza765@gmail.com>
2023-10-28 15:38:16 -0300

    version 5.1

--- js/index.js
+++ js/index.js
@@ -11,6 +11,9 @@ const app = createApp({
     methods:{
         redirection(){
             return window.location.href = "https://instagram.com/sofii_gxn?igshid=YTQwZjQ0NmI0OA=="
+        },
+        whatsapp(){
+            return window.location.href = "https://wa.me/+5493625321242"
         }
     },
     computed:{
